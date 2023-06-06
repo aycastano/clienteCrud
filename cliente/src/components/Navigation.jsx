8539c38e2edd64881ca2454ac8a77c88b7f775f8
Yamid Castaño <aycastano21@gmail.com>
2023-06-06 14:31:14 -0500

    first commit

--- cliente/src/components/Navigation.jsx
+++ cliente/src/components/Navigation.jsx
@@ -4,7 +4,7 @@ export function Navigation(){
     return(
         <div className='flex justify-between py-3'>
             <Link to ="/cliente_crud">
-                <h1 className='font-bold text-3xl mb-4'>cliente APP</h1></Link> 
+                <h1 className='font-bold text-3xl mb-4'>Lista clientes</h1></Link> 
             <button className='bg-indigo-500 px-3 py-2 rounded-lg'>
                 <Link to="/cliente_crud-create">Crear cliente</Link>
             </button>
